7bb58215c3804e760d465d36c9e7c31a6831623e
Piotr Majkrzak <piotr@majkrzak.dev>
2021-08-28 19:15:49 +0200

    Add SPI timing

--- firmware/spi.c
+++ firmware/spi.c
@@ -3,12 +3,19 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#define DELAY 20
+
 void spi_init() {
-  DDRB |= (1 << PB0) | (1 << PB2) | (1 << PB4);
+  DDRB |= 1 << PB0;
+  DDRB |= 1 << PB2;
+  DDRB |= 1 << PB4;
   DDRB &= ~(1 << PB1);
 }
 
-void spi_select() { PORTB &= ~(1 << PB4); }
+void spi_select() {
+  PORTB &= ~(1 << PB4);
+  _delay_ms(DELAY);
+}
 
 void spi_wait() {
   while (PORTB & (1 << PB1))
@@ -28,11 +35,14 @@ uint8_t spi_xfer(uint8_t in) {
     out |= (PINB >> PB0) & 1;
     // tick the clock
     PORTB |= (1 << PB2);
-    _delay_ms(50);
+    _delay_ms(DELAY);
     PORTB &= ~(1 << PB2);
-    _delay_ms(50);
+    _delay_ms(DELAY);
   }
   return out;
 }
 
-void spi_deselect() { PORTB |= (1 << PB4); }
+void spi_deselect() {
+  PORTB |= (1 << PB4);
+  _delay_ms(DELAY);
+}
