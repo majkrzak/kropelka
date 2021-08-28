2fba30c239cc70f5a5b253644b25be1c68fc2959
Piotr Majkrzak <piotr@majkrzak.dev>
2021-08-28 22:30:56 +0200

    Add first test transmission\n rtl_433 -X "name=kropelka,modulation=OOK_MC_ZEROBIT,short=844,l=0,r=1672,preamble=aaabba,get=test:@0:{32}"

--- firmware/main.c
+++ firmware/main.c
@@ -1,1 +1,18 @@
-int main() { return 0; }
+#include "cc1101.h"
+#include "radio.h"
+#include "spi.h"
+#include <util/delay.h>
+
+int main() {
+  spi_init();
+  cc1101_init();
+  radio_init();
+
+  do {
+    for (int i = 0; i < 50; i++)
+      _delay_ms(100);
+    radio_calibrate();
+    radio_send((uint8_t[]){'t', 'e', 's', 't'});
+  } while (1);
+  return 0;
+}
